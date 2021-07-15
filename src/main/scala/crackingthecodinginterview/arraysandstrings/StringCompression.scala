5bc6bb1978ca89e18cb3f48546f2f46d8953acaf
Alex Garland <alex.d.garland@gmail.com>
2021-07-15 16:15:51 +0100

    Refactor out shared code in string compression functions

--- src/main/scala/crackingthecodinginterview/arraysandstrings/StringCompression.scala
+++ src/main/scala/crackingthecodinginterview/arraysandstrings/StringCompression.scala
@@ -3,13 +3,41 @@ package crackingthecodinginterview.arraysandstrings
 object StringCompression {
 
   /***
+   * Shared code to run through a string (in O(n)),
+   * calling a given function with the character and run-length whenever the end of a run of the same char is reached.
+   *
+   * @param s
+   * @param function
+   */
+  private def executeOverCharacterRuns(s: String, function: (Char, Int) => Unit): Unit = {
+    var activeChar: Option[Char] = None
+    var currentSeriesLength: Int = 0
+
+    val applyFunction = { () => activeChar.foreach { charValue => function(charValue, currentSeriesLength) } }
+
+    s.foreach { char =>
+      if(activeChar.contains(char)) {
+        currentSeriesLength += 1
+      }
+      else {
+        applyFunction()
+        activeChar = Some(char)
+        currentSeriesLength = 1
+      }
+    }
+    applyFunction()
+  }
+
+  private def runLengthSaving(seriesLength: Int) = seriesLength - (1 + seriesLength.toString.length)
+
+  /***
    * Compress string by run-length encoding, i.e. aaaaabbbcccccccddd resolves to a5b3c7d3.
    * Return the original string if the compression would not reduce overall length.
    *
    * This version of the function is optimised for time complexity only,
    * running in O(n) as it passes through the string once and uses a StringBuilder.
    *
-   * See below for alternative optimisations targeting the case where a new compressed string is not possible -
+   * See below for alternative optimisations targeting the case where compression cannot reduce length -
    * unless this case is very likely (or the typical savings when it does occur are known to be very significant),
    * this implementation may be the best option due to simplicity if nothing else.
    *
@@ -18,29 +46,14 @@ object StringCompression {
    */
   def compressString(s: String): String = {
     val builder = new StringBuilder()
-    var activeChar: Option[Char] = None
-    var currentSeriesLength: Int = 0
-
-    val flush = { () => activeChar.foreach { charValue => builder.append(s"$charValue$currentSeriesLength") } }
-
-    s.foreach { char =>
-      if(activeChar.contains(char)) {
-        currentSeriesLength += 1
-      }
-      else {
-        flush()
-        activeChar = Some(char)
-        currentSeriesLength = 1
-      }
-    }
-    flush()
+    executeOverCharacterRuns(s, (char: Char, seriesLength: Int) => builder.append(s"$char$seriesLength"))
     val newString = builder.toString()
     if(newString.length < s.length) newString else s
   }
 
   /***
    *  Alternative version which avoids building the final string if not needed.
-   *  This doesn't have many downsides - just tracking the net saving as an int - and can save space in a non-typical case.
+   *  This doesn't have many downsides - just tracking the net saving as an int - and can save space in the non-compressible case.
    *
    *  No change to time complexity (O(n)).
    *
@@ -49,28 +62,14 @@ object StringCompression {
    */
   def compressStringAvoidingNewStringIfNotNeeded(s: String): String = {
     val builder = new StringBuilder()
-    var activeChar: Option[Char] = None
-    var currentSeriesLength: Int = 0
     var netSaving: Int = 0
-
-    def flush(): Unit = {
-      activeChar.foreach { charValue =>
-        builder.append(s"$charValue$currentSeriesLength")
-        netSaving += currentSeriesLength - (1 + currentSeriesLength.toString.length)
+    executeOverCharacterRuns(
+      s,
+      (char, seriesLength) => {
+        builder.append(s"$char$seriesLength")
+        netSaving += runLengthSaving(seriesLength)
       }
-    }
-
-    s.foreach { char =>
-      if(activeChar.contains(char)) {
-        currentSeriesLength += 1
-      }
-      else {
-        flush()
-        activeChar = Some(char)
-        currentSeriesLength = 1
-      }
-    }
-    flush()
+    )
     if(netSaving > 0) builder.toString() else s
   }
 
@@ -85,46 +84,13 @@ object StringCompression {
    * @return
    */
   def compressStringAvoidingStringBuilderIfNotNeeded(s: String): String = {
-
-    var activeChar: Option[Char] = None
-    var currentSeriesLength: Int = 0
     var netSaving: Int = 0
-
-    val incrementNetSaving = () => netSaving += currentSeriesLength - (1 + currentSeriesLength.toString.length)
-
-    s.foreach { char =>
-      if(activeChar.contains(char)) {
-        currentSeriesLength += 1
-      }
-      else {
-        incrementNetSaving()
-        activeChar = Some(char)
-        currentSeriesLength = 1
-      }
-    }
-    incrementNetSaving()
-
+    executeOverCharacterRuns(s, (_, seriesLength) => netSaving += runLengthSaving(seriesLength))
     // If we know there will be no saving, return original string without allocating StringBuilder
     if (netSaving <= 0) { return s }
-
-    // Else reset loop variables and create StringBuilder, then run through the loop again using the builder
-    activeChar = None
-    currentSeriesLength = 0
+    // Else run through the loop again using a builder
     val builder = new StringBuilder()
-
-    val flush = { () => activeChar.foreach { charValue => builder.append(s"$charValue$currentSeriesLength") } }
-
-    s.foreach { char =>
-      if(activeChar.contains(char)) {
-        currentSeriesLength += 1
-      }
-      else {
-        flush()
-        activeChar = Some(char)
-        currentSeriesLength = 1
-      }
-    }
-    flush()
+    executeOverCharacterRuns(s, (char, seriesLength) => builder.append(s"$char$seriesLength"))
     builder.toString()
   }
 
