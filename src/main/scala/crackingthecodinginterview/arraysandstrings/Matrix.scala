2636f6d895fd44157f39d8b9f8e6d8aa8973c174
Alex Garland <alex.d.garland@gmail.com>
2021-07-17 14:01:44 +0100

    Implement in-place matrix rotation
    - Currently only for clockwise - anti-clockwise is TODO
    - Also extended tests to include odd-sized test matrix

--- src/main/scala/crackingthecodinginterview/arraysandstrings/Matrix.scala
+++ src/main/scala/crackingthecodinginterview/arraysandstrings/Matrix.scala
@@ -2,7 +2,18 @@ package crackingthecodinginterview.arraysandstrings
 
 object Matrix {
 
-  case class Indices(rowIndex: Int, ColumnIndex: Int)
+  case class Indices(rowIndex: Int, columnIndex: Int)
+
+  class ExtendedMatrix(matrix: Array[Array[Int]]) {
+    def get(indices: Indices): Int = {
+      matrix(indices.rowIndex)(indices.columnIndex)
+    }
+    def set(indices: Indices, value: Int): Unit = {
+      matrix(indices.rowIndex)(indices.columnIndex) = value
+    }
+  }
+
+  implicit def extendedMatrix(matrix: Array[Array[Int]]): ExtendedMatrix = new ExtendedMatrix(matrix)
 
   sealed trait RotationDirection {
     def getIndices(outer: Int, inner: Int, matrixSize: Int): Indices
@@ -45,6 +56,36 @@ object Matrix {
       outer(i) = inner
     }
     outer
+  }
+
+  /***
+   * Rotate a matrix in place, hence saving space. Runs in O(n squared).
+   *
+   * TODO - make it possible to rotate anti-clockwise as well
+   *
+   * @param matrix
+   */
+  def rotateMatrixInPlace(matrix: Array[Array[Int]]): Unit = {
+    // Outer loop is O(n) (constant multiple of 0.5, ignoring the absolute middle point in n is odd)
+    (0 until matrix.length / 2).foreach { layerOffset =>
+      val lowerBound = layerOffset
+      val upperBound = matrix.length - (layerOffset + 1)
+      // Inner loop runs in O(n), with an average multiple of 0.5
+      (lowerBound until upperBound).foreach { pointOffset =>
+          val topPoint = Indices(lowerBound, lowerBound + pointOffset)
+          val rightPoint = Indices(lowerBound + pointOffset, upperBound)
+          val bottomPoint = Indices(upperBound, upperBound - pointOffset)
+          val leftPoint = Indices(upperBound - pointOffset, lowerBound)
+          // This temp var only ever takes constant space of 1 value
+          val temp = matrix.get(topPoint)
+          // If we're being very precise, note that the four set calls here cancel out the 0.5 factors above
+          // and mean that we get *exactly* n-squared (minus 1 where n is odd) calls
+          matrix.set(topPoint, matrix.get(leftPoint))
+          matrix.set(leftPoint, matrix.get(bottomPoint))
+          matrix.set(bottomPoint, matrix.get(rightPoint))
+          matrix.set(rightPoint, temp)
+      }
+    }
   }
 
 }
--- src/test/scala/crackingthecodinginterview/arraysandstrings/MatrixSpec.scala
+++ src/test/scala/crackingthecodinginterview/arraysandstrings/MatrixSpec.scala
@@ -6,34 +6,77 @@ import org.scalatest.matchers.should
 
 class MatrixSpec extends AnyFlatSpec with should.Matchers {
 
-  // May need to change this if and when we start doing in-place transformations
-  val input: Array[Array[Int]] = Array(
+  private def getInputEvenSize = Array(
     Array(1, 2, 3, 4),
     Array(5, 6, 7, 8),
     Array(9, 10, 11, 12),
     Array(13, 14, 15, 16)
   )
 
-  "rotateMatrix function" should "rotate a matrix by 90 degrees clockwise by default" in {
-    Matrix.rotateMatrix(input) should be(
-      Array(
-        Array(13, 9, 5, 1),
-        Array(14, 10, 6, 2),
-        Array(15, 11, 7, 3),
-        Array(16, 12, 8, 4)
-      )
-    )
+  private val expectedClockwiseEvenSize = Array(
+    Array(13, 9, 5, 1),
+    Array(14, 10, 6, 2),
+    Array(15, 11, 7, 3),
+    Array(16, 12, 8, 4)
+  )
+
+  private val expectedAntiClockwiseEvenSize = Array(
+    Array(4, 8, 12, 16),
+    Array(3, 7, 11, 15),
+    Array(2, 6, 10, 14),
+    Array(1, 5, 9, 13)
+  )
+
+  private def getInputOddSize = Array(
+    Array(1, 2, 3, 4, 5),
+    Array(6, 7, 8, 9, 10),
+    Array(11, 12, 13, 14, 15),
+    Array(16, 17, 18, 19, 20),
+    Array(21, 22, 23, 24, 25)
+  )
+
+  private val expectedClockwiseOddSize = Array(
+    Array(21, 16, 11, 6, 1),
+    Array(22, 17, 12, 7, 2),
+    Array(23, 18, 13, 8, 3),
+    Array(24, 19, 14, 9, 4),
+    Array(25, 20, 15, 10, 5)
+  )
+
+  private val expectedAntiClockwiseOddSize = Array(
+    Array(5, 10, 15, 20, 25),
+    Array(4, 9, 14, 19, 24),
+    Array(3, 8, 13, 18, 23),
+    Array(2, 7, 12, 17, 22),
+    Array(1, 6, 11, 16, 21)
+  )
+
+  "rotateMatrix function" should "produce a copy of a even-sized matrix rotated by 90 degrees clockwise by default" in {
+    Matrix.rotateMatrix(getInputEvenSize) should be(expectedClockwiseEvenSize)
   }
 
-  "rotateMatrix function" should "rotate be able to rotate a matrix by 90 degrees anti-clockwise" in {
-    Matrix.rotateMatrix(input, AntiClockwise) should be(
-      Array(
-        Array(4, 8, 12, 16),
-        Array(3, 7, 11, 15),
-        Array(2, 6, 10, 14),
-        Array(1, 5, 9, 13)
-      )
-    )
+  it should "be able to produce a copy of an even-sized matrix rotated by 90 degrees anti-clockwise" in {
+    Matrix.rotateMatrix(getInputEvenSize, AntiClockwise) should be(expectedAntiClockwiseEvenSize)
+  }
+
+  it should "produce a copy of a odd-sized matrix rotated by 90 degrees clockwise by default" in {
+    Matrix.rotateMatrix(getInputOddSize) should be(expectedClockwiseOddSize)
+  }
+
+  it should "be able to produce a copy of an odd-sized matrix rotated by 90 degrees anti-clockwise" in {
+    Matrix.rotateMatrix(getInputOddSize, AntiClockwise) should be(expectedAntiClockwiseOddSize)
+  }
+
+  "rotateMatrixInPlace function" should "rotate an even-sized matrix by 90 degrees clockwise by default" in {
+    val matrix = getInputEvenSize
+    Matrix.rotateMatrixInPlace(matrix)
+    matrix should be(expectedClockwiseEvenSize)
+  }
+
+  it should "rotate an odd-sized matrix by 90 degrees clockwise by default" in {
+    val matrix = getInputOddSize
+    Matrix.rotateMatrixInPlace(matrix)
+    matrix should be(expectedClockwiseOddSize)
   }
 
 }
