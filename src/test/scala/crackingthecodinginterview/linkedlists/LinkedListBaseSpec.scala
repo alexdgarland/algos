2592d5cd5c303bc4c713f5f7bc55add01982e329
Alex Garland <alex.d.garland@gmail.com>
2021-07-21 19:36:27 +0100

    Clean up test names

--- src/test/scala/crackingthecodinginterview/linkedlists/LinkedListBaseSpec.scala
+++ src/test/scala/crackingthecodinginterview/linkedlists/LinkedListBaseSpec.scala
@@ -16,57 +16,57 @@ class LinkedListBaseSpec extends AnyFlatSpec with should.Matchers {
     LinkedList.fromList(scalaList).toList should be(scalaList)
   }
 
-  it should "be possible to append a value when populated" in {
+  it should "allow appending a value when populated" in {
     val linkedList = LinkedList.fromList(List(1, 2, 3))
     linkedList.append(4)
     linkedList.toList should be(List(1, 2, 3, 4))
   }
 
-  it should "be possible to append a value when not populated" in {
+  it should "allow appending a value when not populated" in {
     val linkedList = LinkedList.fromList(List())
     linkedList.append(4)
     linkedList.toList should be(List(4))
   }
 
-  it should "be possible to insert a value within list when populated" in {
+  it should "allow inserting a value within list when populated" in {
     val linkedList = LinkedList.fromList(List(1, 2, 3))
     linkedList.insert(4, 2)
     linkedList.toList should be(List(1, 2, 4, 3))
   }
 
-  it should "be possible to insert a value to start of list when populated" in {
+  it should "allow inserting a value to start of list when populated" in {
     val linkedList = LinkedList.fromList(List(1, 2, 3))
     linkedList.insert(4, 0)
     linkedList.toList should be(List(4, 1, 2, 3))
   }
 
-  it should "be possible to insert a value to end of list when populated" in {
+  it should "allow inserting a value to end of list when populated" in {
     val linkedList = LinkedList.fromList(List(1, 2, 3))
     linkedList.insert(4, 3)
     linkedList.toList should be(List(1, 2, 3, 4))
   }
 
-  it should "be possible to insert a value when not populated" in {
+  it should "allow inserting a value when not populated" in {
     val linkedList = LinkedList.fromList(List())
     linkedList.insert(4, 0)
     linkedList.toList should be(List(4))
   }
 
-  it should "not be possible to insert a value at a negative index" in {
+  it should "not allow insertion of a value at a negative index" in {
     val linkedList = LinkedList.fromList(List(1, 2, 3))
     assertThrows[IndexOutOfBoundsException] {
       linkedList.insert(4, -1)
     }
   }
 
-  it should "not be possible to insert a value at an index beyond the end of a populated list" in {
+  it should "not allow insertion of a value at an index beyond the end of a populated list" in {
     val linkedList = LinkedList.fromList(List(1, 2, 3))
     assertThrows[IndexOutOfBoundsException] {
       linkedList.insert(4, 4)
     }
   }
 
-  it should "not be possible to insert a value at an index beyond the end of an empty list" in {
+  it should "not allow insertion of a value at an index beyond the end of an empty list" in {
     val linkedList = LinkedList.fromList(List())
     assertThrows[IndexOutOfBoundsException] {
       linkedList.insert(4, 1)
