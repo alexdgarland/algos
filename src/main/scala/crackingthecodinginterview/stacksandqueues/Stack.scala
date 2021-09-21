aa566da3a804b3fc70f9acfb60bbdd97fbd5d576
Alex Garland <alex.d.garland@gmail.com>
2021-09-21 15:04:50 +0100

    Basic Stack implementation using SinglyLinkedList

---
+++ src/main/scala/crackingthecodinginterview/stacksandqueues/Stack.scala
@@ -0,0 +1,44 @@
+package crackingthecodinginterview.stacksandqueues
+
+import crackingthecodinginterview.linkedlists.SinglyLinkedList
+
+trait Stack[T] {
+
+  def push(value: T): Unit
+
+  def pop(): Option[T]
+
+  def peek(): Option[T]
+
+  def isEmpty: Boolean
+
+  def toList: List[T]
+}
+
+object Stack {
+  def apply[T](implicit ordering: Ordering[T]): Stack[T] = new LinkedListStack[T]()
+
+  private def apply[T](list: SinglyLinkedList[T])(implicit ordering: Ordering[T]): Stack[T] = {
+    val stack = new LinkedListStack[T]()
+    stack.head = list.head
+    stack
+  }
+
+  def apply[T](values: T*)(implicit ordering: Ordering[T]): Stack[T] = Stack(SinglyLinkedList(values: _*))
+}
+
+class LinkedListStack[T](implicit ordering: Ordering[T]) extends SinglyLinkedList[T] with Stack[T] {
+  override def push(value: T): Unit = prepend(value)
+
+  override def pop(): Option[T] = {
+    val poppedValue = head.map(_.value)
+    head.foreach(currentHead => head = currentHead.next)
+    poppedValue
+  }
+
+  override def peek(): Option[T] = head.map(_.value)
+
+  override def isEmpty: Boolean = head.isEmpty
+
+  override def toList: List[T] = toList()
+}
