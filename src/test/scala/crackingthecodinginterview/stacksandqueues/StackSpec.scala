da998c7986434070a7820fe06bc0018afab9bead
Alex Garland <alex.d.garland@gmail.com>
2021-09-30 11:09:11 +0100

    Implement method to sort Stack

--- src/test/scala/crackingthecodinginterview/stacksandqueues/StackSpec.scala
+++ src/test/scala/crackingthecodinginterview/stacksandqueues/StackSpec.scala
@@ -9,4 +9,10 @@ class StackSpec extends AnyFlatSpec with should.Matchers with SharedStackBehavio
 
   "default stack implementation" should behave like defaultStack(newDefaultStack)
 
+  "sort method" should "sort a stack as expected" in {
+    val stack = Stack(5, 2, 7, 2, 3, 9)
+    Stack.sort(stack)
+    stack.toList should be(List(2, 2, 3, 5, 7, 9))
+  }
+
 }
