e00b34e01051bb384f692938391a47aea9b3a7ee
Alex Garland <alex.d.garland@gmail.com>
2021-10-28 22:34:32 +0100

    Update TODO comments
    Describing current thoughts on possible implementation of limit on required suggestions from Trie

--- src/main/scala/crackingthecodinginterview/treesandgraphs/trie/NodeTraits.scala
+++ src/main/scala/crackingthecodinginterview/treesandgraphs/trie/NodeTraits.scala
@@ -70,36 +70,36 @@ private trait ListChildNodeStore[N <: ListChildNodeStore[_]] extends TrieNode[N]
 /*
 SUGGESTION STRATEGIES
 
-There are two obvious ways to do it:
-
-- Recalculate on the fly each time by traversing down the sub-trie -
-    that is more expensive at read-time but cheaper at write time and in terms of memory
-- Save the full list of words at each prefix node -
-    faster to read but will need updating on each write and could use a LOT of memory.
-
-TODO (potentially): further optimisations could involve returning only a subset of the possible words:
-  - Performance-focused - stop looking as soon as have found _n_ arbitrarily-selected (first available) matches.
-  - Functionality-focused - could return the most likely/ appropriate words -
-    but this would involve some depth of useful data around relative usage/ probability
-    (which I guess we could kind of fake but not necessarily very useful relative to the impl complexity??)
-    or maybe doing something like a full-on Markov chain (???) which is way out of scope for right now...
-    Could also make it not lose its place in the Trie as new letters are submitted in a real-time text stream -
-    but that could get fairly complex.
-
+There are two obvious ways to do it, as per the two alternative traits below.
  */
 
+/**
+ * - Recalculate on the fly each time by traversing down the sub-trie -
+ *  that is more expensive at read-time but cheaper at write time and (particularly) in terms of memory.
+ */
 private trait SuggestionBuilder {
 
+  // TODO - could potentially allow limiting to only return a maximum of n words (first found in an arbitrary order),
+  //  which could terminate the build process early in some cases and hence improve performance
+  //  The most performant approach would go depth-first which would result in suggestions coming back in alphabetical order;
+  //  the equivalent contract could also be implemented for the store below, although this be unlikely to improve performance (probably the reverse).
   def buildSuggestions(prefix: String): List[String]
 
 }
 
+/**
+ * - Save the full list of words at each prefix node -
+ *  faster to read but will need updating on each write and could use a LOT of memory.
+ */
 private trait SuggestionsStore {
 
   protected val suggestions: mutable.Set[String] = mutable.Set[String]()
 
   def addSuggestion(suggestion: String): Unit = suggestions.add(suggestion)
 
+  // Could potentially add info to allow returning the most relevant suggestions first but this could be a bit complex:
+  //  - to truly understand relevance, we need some kind of deep dataset
+  //    that would almost certainly extend beyond the lifetime of the in-memory data structure.
   def getSuggestions: List[String] = suggestions.toList
 
 }
