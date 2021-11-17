9b85e28342df295edb94d2f8175f25cbb5bbe40d
Alex Garland <alex.d.garland@gmail.com>
2021-11-17 16:55:16 +0000

    WIP - slightly clean up ChildListNodeWithSuggestionBuilding impl
    (still imperative)

--- src/main/scala/crackingthecodinginterview/treesandgraphs/trie/ChildListTrieWithSuggestionBuilding.scala
+++ src/main/scala/crackingthecodinginterview/treesandgraphs/trie/ChildListTrieWithSuggestionBuilding.scala
@@ -14,20 +14,17 @@ private case class ChildListNodeWithSuggestionBuilding(override val storedChar: 
   }
 
   private def innerBuildSuggestions(prefix: String, maxNumberOfSuggestions: Option[Int]): List[String] = {
-    if (maxNumberOfSuggestions.exists(_ <= 0)) return List()
+    if (maxNumberOfSuggestions.contains(0)) return List()
     storedChar.map { char =>
       val nextPrefix = prefix + char
       var suggestions = if(endsValidWord) List(nextPrefix) else List()
-      var remainingSuggestions = maxNumberOfSuggestions.map(s => s - (if(endsValidWord) 1 else 0))
       children
         // If we DON'T care exactly what order we use to pick suggestions, this sort is wasted
         .sortBy(_.storedChar.get)
         .foreach { child =>
-          if (remainingSuggestions.forall(_ > 0)) {
-            val childSuggestions = child.innerBuildSuggestions(nextPrefix, remainingSuggestions)
-            val numChildSuggestions = remainingSuggestions.getOrElse(childSuggestions.length)
-            suggestions = suggestions ::: childSuggestions.take(numChildSuggestions)
-            remainingSuggestions = remainingSuggestions.map(_ - childSuggestions.length)
+          val remainingSuggestions = maxNumberOfSuggestions.map(s => s - suggestions.length)
+          if (!remainingSuggestions.contains(0)) {
+            suggestions = suggestions ::: child.innerBuildSuggestions(nextPrefix, remainingSuggestions)
           }
         }
       suggestions
