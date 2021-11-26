9621d566647a9b98d190ae53fb3d0c3efc9a2e1a
Alex Garland <alex.d.garland@gmail.com>
2021-11-26 14:54:57 +0000

    Complete search impls (DFS/ BFS) for GraphNode class
    - Make DFS iterative rather than recursive (avoids potential stack overflow)
    - General clean-up

--- src/main/scala/crackingthecodinginterview/treesandgraphs/GraphNode.scala
+++ src/main/scala/crackingthecodinginterview/treesandgraphs/GraphNode.scala
@@ -2,44 +2,68 @@ package crackingthecodinginterview.treesandgraphs
 
 import scala.collection.mutable
 
+private case class NodeStack[T](private var list: List[GraphNode[T]] = List[GraphNode[T]]()) {
+
+  def push(node: GraphNode[T]): Unit = list = node :: list
+
+  def pop(): GraphNode[T] = {
+    val node::newList = list
+    list = newList
+    node
+  }
+
+  def nonEmpty: Boolean = list.nonEmpty
+
+}
+
+private case class SeenNodeTracker(private val set: mutable.HashSet[Int] = mutable.HashSet[Int]()) {
+  def addSeen(node: GraphNode[_]): Unit = set.add(node.hashCode())
+
+  def hasBeenSeen(node: GraphNode[_]): Boolean = set.contains(node.hashCode())
+}
+
 class GraphNode[T]
 (
   val value: T,
   val children: mutable.MutableList[GraphNode[T]] = mutable.MutableList[GraphNode[T]]()
 ) {
 
-  def depthFirstSearch(predicate: T => Boolean): Option[T] = depthFirstSearch(predicate, mutable.HashSet[Int]())
+  override def toString: String = s"${super.toString} - value $value"
 
-  private def depthFirstSearch
-  (
-    predicate: T => Boolean,
-    seenNodes: mutable.HashSet[Int]
-  ): Option[T] = {
-    if(predicate(value))
-      return Some(value)
-    children.foreach { node =>
-      if(!seenNodes.contains(node.hashCode())) {
-        seenNodes.add(node.hashCode())
-        // TODO (maybe) - this can overflow the stack due to recursion
-        val innerSearchResult = node.depthFirstSearch(predicate, seenNodes)
-        if (innerSearchResult.isDefined)
-          return innerSearchResult
+  def depthFirstSearch(predicate: T => Boolean): Option[T] = {
+    val stack = NodeStack[T]()
+    stack.push(this)
+    val seenNodes = SeenNodeTracker()
+    while(stack.nonEmpty) {
+      val currentNode = stack.pop()
+      if(!seenNodes.hasBeenSeen(currentNode)) {
+        seenNodes.addSeen(currentNode)
+        if (predicate(currentNode.value))
+          return Some(currentNode.value)
+        currentNode
+          .children
+          .reverse
+          .foreach { stack.push }
       }
     }
     None
   }
 
   def breadthFirstSearch(predicate: T => Boolean): Option[T] = {
-    val seenNodes = mutable.HashSet[Int]()
     val queue = mutable.Queue[GraphNode[T]]()
     queue.enqueue(this)
+    val seenNodes = SeenNodeTracker()
     while(queue.nonEmpty) {
       val currentNode = queue.dequeue()
-      if(!seenNodes.contains(currentNode.hashCode())) {
-        seenNodes.add(currentNode.hashCode())
+      if(!seenNodes.hasBeenSeen(currentNode)) {
+        seenNodes.addSeen(currentNode)
         if(predicate(currentNode.value))
           return Some(currentNode.value)
-        currentNode.children.foreach(queue.enqueue(_))
+        currentNode
+          .children
+          // Filter is slightly redundant functionally, but reduces queue space usage
+          .filter { childNode => !seenNodes.hasBeenSeen(childNode) }
+          .foreach { queue.enqueue(_) }
       }
     }
     None
